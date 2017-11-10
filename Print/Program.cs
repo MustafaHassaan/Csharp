0e3e12a0c7315f5bddf841e928aada8c698f1c6e
Mustafa Hassaan <Mustafa Hassaan@DESKTOP-1OQ83LG>
2017-11-10 15:51:00 +0200

    Add project files.

---
+++ Print/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello worlled");
+            Console.ReadLine();
+        }
+    }
+}
